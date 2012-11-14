6de5ab0edd7852e02b81db2327f5c475e0a70c89
aarmea <alvin.armea@gmail.com>
2012-11-14 17:47:25 -0500

    Split the TileSet class
    Created a TextureAtlas class that handles textures using the
    coordinates in the texture. The TileSet is now a subclass of
    TextureAtlas and works as before.

--- src/levelobj.py
+++ src/levelobj.py
@@ -8,7 +8,38 @@ from texture import Texture
 
 EPSILON = 0.01
 
-class TileSet(object):
+class TextureAtlas(object):
+  """An interface for texturing quads with tiles from a single texture"""
+
+  def __init__(self, filename, tilew, tileh):
+    # Load the texture
+    self.__texture = Texture(filename)
+    
+    # Set the size of each texture
+    self._tilew = tilew
+    self._tileh = tileh
+
+    # Calculate placement parameters
+    self.__es = 1.0 / self.__texture.w
+    self.__et = 1.0 / self.__texture.h
+    self.__htiles = self.__texture.w / self._tilew
+    self.__vtiles = self.__texture.h / self._tileh
+    self.__ntiles = self.__htiles * self.__vtiles
+
+  def bind(self):
+    """Bind this tile set."""
+    self.__texture.bind()
+
+  def tileCoord(self, s, t, u, v):
+    """Set the texture coordinates for the given tile."""
+    assert s < self.__htiles
+    assert t < self.__vtiles
+    assert u >= 0.0 and u <= 1.0
+    assert v >= 0.0 and v <= 1.0
+    glTexCoord2f((s + u) * self._tilew * self.__es,
+      (self.__texture.h - (t + 1.0 - v) * self._tileh) * self.__et)
+
+class TileSet(TextureAtlas):
   """An interface for texturing quads with tiles from a single texture"""
 
   def __init__(self, path, prefix):
@@ -27,33 +58,17 @@ class TileSet(object):
 
       file_.close()
 
-    # Load the texture
-    self.__texture = Texture(path + prefix + "-0.png")
-
     # Pick a sprite and assume all the other sprites are the same size
-    self.__tilew, self.__tileh = self.__images.values()[0][2]
+    tilew, tileh = self.__images.values()[0][2]
 
-    # Calculate placement parameters
-    self.__es = 1.0 / self.__texture.w
-    self.__et = 1.0 / self.__texture.h
-    self.__htiles = self.__texture.w / self.__tilew
-    self.__vtiles = self.__texture.h / self.__tileh
-    self.__ntiles = self.__htiles * self.__vtiles
-
-  def bind(self):
-    """Bind this tile set."""
-    self.__texture.bind()
+    # Call the parent's constructor
+    super(TileSet, self).__init__(path+prefix+"-0.png", tilew, tileh)
 
   def tileCoord(self, spriteName, u, v):
     """Set the texture coordinates for the given tile."""
-    s = self.__images[spriteName][1][0] / self.__tilew
-    t = self.__images[spriteName][1][1] / self.__tileh
-    assert s < self.__htiles
-    assert t < self.__vtiles
-    assert u >= 0.0 and u <= 1.0
-    assert v >= 0.0 and v <= 1.0
-    glTexCoord2f((s + u) * self.__tilew * self.__es,
-      (self.__texture.h - (t + 1.0 - v) * self.__tileh) * self.__et)
+    s = self.__images[spriteName][1][0] / self._tilew
+    t = self.__images[spriteName][1][1] / self._tileh
+    super(TileSet, self).tileCoord(s, t, u, v)
 
 class LevelObject(object):
   """The base level object"""
