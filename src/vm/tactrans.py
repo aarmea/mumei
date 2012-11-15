61f74a1be0eda8aa3e3806e3f9eb0c2ca44c5836
Anthony DeRossi <ajderossi@gmail.com>
2012-11-15 14:33:56 -0500

    Add new TAC instructions and improve bytecode translation
    This commit adds the following three-address code instructions to
    the compiler backend along with bytecode translation support:
    
    - AddressOf
    - Load
    - Store
    - LessThan
    
    This commit also implements translation support for the Call
    instruction, adds typed variables to indicate whether a variable is
    a parameter, local, or global, and cleans up the translation code
    in general.
    
    The AutoGen helper class is also updated to support object
    construction via a user-supplied function.

--- src/vm/tactrans.py
+++ src/vm/tactrans.py
@@ -2,48 +2,89 @@
 A translator from three-address code to bytecode
 """
 
-import bytecode
+from bytecode import *
+
+# Some threeaddr classes have naming conflicts with the bytecode module
+# (e.g. Add), so this has to be a named import.
 import threeaddr.threeaddr as threeaddr
 
-PATCH_WORD = 0xCCCC
+PATCH_WORD = 0xAAAA
+"""A placeholder value for patch locations"""
 
 def _negateWord(val):
   """Negate a processor word."""
-  return -val & bytecode.Processor.WORD_MASK
+  return -val & Processor.WORD_MASK
 
 def _reserveLocal(localVars, localOff, var):
   """Reserve stack space for a local variable if necessary."""
-  if not var in localVars:
-    localVars[var] = localOff
-    localOff += 1
+  if isinstance(var, threeaddr.LocalVar):
+    if not var.id in localVars:
+      localVars[var.id] = localOff
+      localOff += 1
+
   return localOff
 
 def _getOperand(localVars, patches, patchOff, var):
-  """Return an operand for the given variable name, adding an entry to the
-  patch list as necessary."""
-  if var in localVars:
-    # Look up the variable in the local environment
-    off = localVars[var] + 1
-    return (bytecode.IndRegOffOperand(bytecode.Processor.REG_X3,
-      _negateWord(off)), patchOff + 1)
-  else:
-    # Generate a patch to be resolved later
+  """Return an operand for the given variable, adding an entry to the patch
+  list as necessary."""
+  if isinstance(var, int):
+    # Emit the constant directly
+    return (ImmOperand(var), patchOff + 1)
+  elif isinstance(var, str):
+    # Generate a patch for the given label
     patches.append((var, patchOff + 1))
-    return (bytecode.IndImmOperand(PATCH_WORD), patchOff + 1)
+    return (ImmOperand(PATCH_WORD), patchOff + 1)
+  elif isinstance(var, threeaddr.ParamVar):
+    # Get the variable on the stack
+    return (IndRegOffOperand(Processor.REG_X3, var.off + 2),
+      patchOff + 1)
+  elif isinstance(var, threeaddr.LocalVar):
+    # Look up the variable in the local environment
+    off = localVars[var.id] + 1
+    return (IndRegOffOperand(Processor.REG_X3,
+      _negateWord(off)), patchOff + 1)
+  elif isinstance(var, threeaddr.GlobalVar):
+    # Generate a patch to be resolved later
+    patches.append((var.id, patchOff + 1))
+    return (IndImmOperand(PATCH_WORD), patchOff + 1)
+  else:
+    # Not a valid variable type
+    assert False
 
-def _getTarget(patches, patchOff, label):
-  """Return an operand for the given jump target, adding an entry to the patch
-  list."""
-  patches.append((label, patchOff + 1))
-  return bytecode.ImmOperand(0)
+def _getAddrOperand(localVars, patches, patchOff, var):
+  """Return an operand for the address of the given variable, adding an entry to
+  the patch list as necessary."""
+  if isinstance(var, int):
+    # Emit the constant directly
+    return (ImmOperand(var), patchOff + 1)
+  elif isinstance(var, str):
+    # Generate a patch for the given label
+    patches.append((var, patchOff + 1))
+    return (ImmOperand(PATCH_WORD), patchOff + 1)
+  elif isinstance(var, threeaddr.ParamVar):
+    # Get the variable on the stack
+    return (RegOffOperand(Processor.REG_X3, var.off + 2),
+      patchOff + 1)
+  elif isinstance(var, threeaddr.LocalVar):
+    # Look up the variable in the local environment
+    off = localVars[var.id] + 1
+    return (RegOffOperand(Processor.REG_X3, _negateWord(off)),
+      patchOff + 1)
+  elif isinstance(var, threeaddr.GlobalVar):
+    # Generate a patch to be resolved later
+    patches.append((var.id, patchOff + 1))
+    return (ImmOperand(PATCH_WORD), patchOff + 1)
+  else:
+    # Not a valid variable
+    assert False
 
 def translate(tac):
   """Translate a three-address code sequence to a bytecode sequence."""
-  bc = []
+  code = []
 
   def appendInst(inst):
     """Encode an instruction and append the resulting bytecode."""
-    bc.extend(inst.encode())
+    code.extend(inst.encode())
 
   # Second pass information
   labels = {}
@@ -54,134 +95,238 @@ def translate(tac):
   for inst in tac:
     if isinstance(inst, threeaddr.Label):
       # Add the label name and current offset to the label map
-      labels[inst.name] = len(bc)
+      labels[inst.id] = len(code)
 
     elif isinstance(inst, threeaddr.Word):
       # Emit a word directly
-      bc.append(inst.val)
+      code.append(inst.val)
 
     elif isinstance(inst, threeaddr.BeginFunc):
       # Reset local variables
       localVars = {}
       localOff = 0
 
+      # Save the caller's base pointer
+      appendInst(
+        Set(IndDecRegOperand(Processor.REG_SP), RegOperand(Processor.REG_X3))
+      )
+
       # Save the stack pointer
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_X3),
-          bytecode.RegOperand(bytecode.Processor.REG_SP)
-        )
+        Set(RegOperand(Processor.REG_X3), RegOperand(Processor.REG_SP))
       )
 
       # Reserve stack space for local variables
       if inst.nwords:
         appendInst(
-          bytecode.Add(
-            bytecode.RegOperand(bytecode.Processor.REG_SP),
-            bytecode.ImmOperand(_negateWord(inst.nwords))
+          Add(
+            RegOperand(Processor.REG_SP),
+            ImmOperand(_negateWord(inst.nwords))
           )
         )
 
     elif isinstance(inst, threeaddr.EndFunc):
+      retOp, _ = _getOperand(localVars, patches, len(code), inst.ret)
+
       # Set the return value
-      op, _ = _getOperand(localVars, patches, len(bc), inst.ret)
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_X0),
-          op
-        )
+        Set(RegOperand(Processor.REG_X0), retOp)
       )
 
       # Restore the stack pointer
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_SP),
-          bytecode.RegOperand(bytecode.Processor.REG_X3)
-        )
+        Set(RegOperand(Processor.REG_SP), RegOperand(Processor.REG_X3))
+      )
+
+      # Restore the base pointer
+      appendInst(
+        Set(RegOperand(Processor.REG_X3), IndIncRegOperand(Processor.REG_SP))
       )
 
       # Pop the return address and jump to it
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_IP),
-          bytecode.IndIncRegOperand(bytecode.Processor.REG_SP)
-        )
+        Set(RegOperand(Processor.REG_IP), IndIncRegOperand(Processor.REG_SP))
       )
 
     elif isinstance(inst, threeaddr.Assign):
       localOff = _reserveLocal(localVars, localOff, inst.dst)
 
-      dstOp, patchOff = _getOperand(localVars, patches, len(bc), inst.dst)
-      if isinstance(inst.src, int):
-        srcOp = bytecode.ImmOperand(inst.src)
-      else:
-        srcOp, _ = _getOperand(localVars, patches, patchOff, inst.src)
-      appendInst(bytecode.Set(dstOp, srcOp))
+      dstOp, patchOff = _getOperand(localVars, patches, len(code), inst.dst)
+      srcOp, _ = _getOperand(localVars, patches, patchOff, inst.src)
+      appendInst(Set(dstOp, srcOp))
+
+    elif isinstance(inst, threeaddr.Load):
+      localOff = _reserveLocal(localVars, localOff, inst.dst)
+
+      srcOp, _ = _getOperand(localVars, patches, len(code), inst.src)
+
+      # Load the source address into a register
+      appendInst(
+        Set(RegOperand(Processor.REG_X0), srcOp)
+      )
+
+      dstOp, patchOff = _getOperand(localVars, patches, len(code), inst.dst)
+
+      # Read the value at the source address into the destination
+      appendInst(
+        Set(dstOp, IndRegOperand(Processor.REG_X0))
+      )
+
+    elif isinstance(inst, threeaddr.AddressOf):
+      localOff = _reserveLocal(localVars, localOff, inst.dst)
+      localOff = _reserveLocal(localVars, localOff, inst.src)
+
+      dstOp, patchOff = _getOperand(localVars, patches, len(code), inst.dst)
+      srcOp, _ = _getAddrOperand(localVars, patches, patchOff, inst.src)
+
+      # Set the destination variable to the address of the source variable
+      appendInst(Set(dstOp, srcOp))
+
+    elif isinstance(inst, threeaddr.Store):
+      dstOp, _ = _getOperand(localVars, patches, len(code), inst.dst)
+
+      # Load the destination address into a register
+      appendInst(
+        Set(RegOperand(Processor.REG_X0), dstOp)
+      )
+
+      srcOp, _ = _getOperand(localVars, patches, len(code), inst.src)
+
+      # Store the source value at the destination address
+      appendInst(
+        Set(IndRegOperand(Processor.REG_X0), srcOp)
+      )
+
+    elif isinstance(inst, threeaddr.PushParam):
+      srcOp, _ = _getOperand(localVars, patches, len(code), inst.src)
+
+      # Push the source value on the stack
+      appendInst(
+        Set(IndDecRegOperand(Processor.REG_SP), srcOp)
+      )
+
+    elif isinstance(inst, threeaddr.PopParams):
+      # Remove nwords values from the stack
+      appendInst(
+        Add(RegOperand(Processor.REG_SP), ImmOperand(inst.nwords))
+      )
+
+    elif isinstance(inst, threeaddr.Call):
+      localOff = _reserveLocal(localVars, localOff, inst.dst)
+
+      targetOp, _ = _getOperand(localVars, patches, len(code), inst.target)
+
+      # Generate a label name and patch for the return address
+      retLabel = "#ret%x" % len(code)
+      patches.append((retLabel, len(code) + 1))
+
+      # Push the return address on the stack
+      appendInst(
+        Set(IndDecRegOperand(Processor.REG_SP), ImmOperand(PATCH_WORD))
+      )
+
+      # Jump to the given address
+      appendInst(
+        Set(RegOperand(Processor.REG_IP), targetOp)
+      )
+
+      # Label the return address
+      labels[retLabel] = len(code)
+
+      # Get the return value
+      dstOp, _ = _getOperand(localVars, patches, len(code), inst.dst)
+      appendInst(
+        Set(dstOp, RegOperand(Processor.REG_X0))
+      )
+
+    elif isinstance(inst, threeaddr.LessThan):
+      localOff = _reserveLocal(localVars, localOff, inst.dst)
+
+      srcOpA, _ = _getOperand(localVars, patches, len(code), inst.srca)
+
+      # Copy the first variable into X0
+      appendInst(
+        Set(RegOperand(Processor.REG_X0), srcOpA)
+      )
+
+      srcOpB, _ = _getOperand(localVars, patches, len(code), inst.srcb)
+
+      # Subtract the second variable from X0
+      appendInst(
+        Sub(RegOperand(Processor.REG_X0), srcOpB)
+      )
+
+      # Mask the less-than flag in the flags register
+      appendInst(
+        And(RegOperand(Processor.REG_FL), ShortImmOperand(Processor.FLAG_LESS))
+      )
+
+      dstOp, _ = _getOperand(localVars, patches, len(code), inst.dst)
+
+      # Copy the flags register to the destination variable
+      appendInst(
+        Set(dstOp, RegOperand(Processor.REG_FL))
+      )
 
     elif isinstance(inst, threeaddr.Add):
       localOff = _reserveLocal(localVars, localOff, inst.dst)
 
-      op, _ = _getOperand(localVars, patches, len(bc), inst.srca)
+      srcOpA, _ = _getOperand(localVars, patches, len(code), inst.srca)
+
+      # Load the first variable into X0
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_X0),
-          op
-        )
+        Set(RegOperand(Processor.REG_X0), srcOpA)
       )
-      op, _ = _getOperand(localVars, patches, len(bc), inst.srcb)
+
+      srcOpB, _ = _getOperand(localVars, patches, len(code), inst.srcb)
+
+      # Add the second variable to X0
       appendInst(
-        bytecode.Add(
-          bytecode.RegOperand(bytecode.Processor.REG_X0),
-          op
-        )
+        Add(RegOperand(Processor.REG_X0), srcOpB)
       )
-      op, _ = _getOperand(localVars, patches, len(bc),  inst.dst)
+
+      dstOp, _ = _getOperand(localVars, patches, len(code), inst.dst)
+
+      # Copy X0 into the destination variable
       appendInst(
-        bytecode.Set(
-          op,
-          bytecode.RegOperand(bytecode.Processor.REG_X0)
-        )
+        Set(dstOp, RegOperand(Processor.REG_X0))
       )
 
     elif isinstance(inst, threeaddr.Jump):
+      targetOp, _ = _getOperand(localVars, patches, len(code), inst.target)
+
+      # Jump to the given address
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_IP),
-          _getTarget(patches, len(bc), inst.label)
-        )
+        Set(RegOperand(Processor.REG_IP), targetOp)
       )
 
     elif isinstance(inst, threeaddr.IfZeroJump):
+      srcOp, _ = _getOperand(localVars, patches, len(code), inst.src)
+
       # Set the flags register
-      op, _ = _getOperand(localVars, patches, len(bc), inst.src)
       appendInst(
-        bytecode.Or(
-          bytecode.ShortImmOperand(0),
-          op
-        )
+        Or(ShortImmOperand(0), srcOp)
       )
 
       # Check the flags register
       appendInst(
-        bytecode.If(
-          bytecode.RegOperand(bytecode.Processor.REG_FL),
-          bytecode.ShortImmOperand(bytecode.Processor.FLAG_ZERO)
-        )
+        If(RegOperand(Processor.REG_FL), ShortImmOperand(Processor.FLAG_ZERO))
       )
 
-      # Jump to the target
+      targetOp, _ = _getOperand(localVars, patches, len(code), inst.target)
+
+      # Jump to the given address
       appendInst(
-        bytecode.Set(
-          bytecode.RegOperand(bytecode.Processor.REG_IP),
-          _getTarget(patches, len(bc), inst.label)
-        )
+        Set(RegOperand(Processor.REG_IP), targetOp)
       )
 
     else:
       raise NotImplementedError(inst) # XXX
 
-  # Resolve patches
+  # Go through the list of patches and substitute in the correct label
+  # addresses.
   for label, off in patches:
-    bc[off] = labels[label]
+    assert code[off] == PATCH_WORD
+    code[off] = labels[label]
 
-  return bc
+  return code
