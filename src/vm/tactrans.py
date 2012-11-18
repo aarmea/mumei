d772a40acb74155f5b92f2066c519076bbfafa63
Anthony DeRossi <ajderossi@gmail.com>
2012-11-18 04:56:14 -0500

    Implement a TAC greater-than instruction
    This commit includes bytecode translation support.

--- src/vm/tactrans.py
+++ src/vm/tactrans.py
@@ -424,6 +424,50 @@ def translate(tac):
         Set(dstOp, RegOperand(Processor.REG_FL))
       )
 
+    elif isinstance(inst, threeaddr.GreaterThan):
+      localOff = _reserveLocal(localVars, localOff, inst.dst)
+
+      srcOpA, _ = _getOperand(localVars, patches, len(code), inst.srca)
+
+      # Copy the first variable into X0
+      appendInst(
+        Set(RegOperand(Processor.REG_X0), srcOpA)
+      )
+
+      srcOpB, _ = _getOperand(localVars, patches, len(code), inst.srcb)
+
+      # Subtract the second variable from X0
+      appendInst(
+        Sub(RegOperand(Processor.REG_X0), srcOpB)
+      )
+
+      # Mask the less-than and zero flags in the flags register
+      appendInst(
+        And(
+          RegOperand(Processor.REG_FL),
+          ShortImmOperand(Processor.FLAG_LESS | Processor.FLAG_ZERO)
+        )
+      )
+
+      # Copy the flags temporarily
+      # XXX The flags register should probably be updated if it isn't the
+      # destination operand of an instruction.
+      appendInst(
+        Set(RegOperand(Processor.REG_X3), RegOperand(Processor.REG_FL))
+      )
+
+      # Check whether the flags are zero
+      appendInst(
+        Or(RegOperand(Processor.REG_X3), ShortImmOperand(0))
+      )
+
+      dstOp, _ = _getOperand(localVars, patches, len(code), inst.dst)
+
+      # Copy the flags register to the destination variable
+      appendInst(
+        Set(dstOp, RegOperand(Processor.REG_FL))
+      )
+
     elif isinstance(inst, threeaddr.Neg):
       localOff = _reserveLocal(localVars, localOff, inst.dst)
 
