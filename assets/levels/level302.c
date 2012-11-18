17e4a0698f4b7d5fe8625e064f451d4faea19336
Rachel.Redner <rachelshaeredner@gmail.com>
2012-11-18 12:22:42 -0500

    added all of level 3

---
+++ assets/levels/level302.c
@@ -0,0 +1,14 @@
+int moveX(int distance);
+extern int FloorTileExists = 1;
+int wait();
+extern int doorcolor;
+extern int color;
+
+int main ()
+{
+  int color = 0;
+  /* Your code here */
+
+
+  return 0;
+}
