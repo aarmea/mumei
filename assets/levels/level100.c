21a7785ba6afeb85d49f7a49c9a6591c2bf0d59d
Albert Armea <alvin.armea@gmail.com>
2012-12-14 14:21:46 -0500

    Made level 100 more playable

--- assets/levels/level100.c
+++ assets/levels/level100.c
@@ -1,6 +1,20 @@
+/*
+Level 1: variable assignment
+
+Use the arrow keys to move around the editor.
+Press Alt+(arrow key) to move your character.
+
+Notice that the robot is already at the door.
+
+Every C program needs a main() function, which
+is where the program starts when it is run.
+We're starting you off with one.
+Fill it in with code and press F5.
+*/
+
 int main ()
 {
-  moveX(2);
-  moveX(-2);
+  /* Insert your code here */
+  
   return 0;
 }
