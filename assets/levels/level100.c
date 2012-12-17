07d7c56a8824d313409063c22e7562da3cf06ac7
Albert Armea <alvin.armea@gmail.com>
2012-12-16 21:50:12 -0500

    Made level 101 more playable
    Added some comments to the provided user code and changed the
    default cursor location to below the "/* Insert your ... */"

--- assets/levels/level100.c
+++ assets/levels/level100.c
@@ -1,20 +1,33 @@
 /*
-Level 1: variable assignment
+Level 1-0: variable assignment
 
 Use the arrow keys to move around the editor.
-Press Alt+(arrow key) to move your character.
+Press Alt+[arrow key] to move your character.
+
+The goal of these levels is to stop the robot
+at the door and change its eye color to match
+the color on the door.
 
 Notice that the robot is already at the door.
+
+You can assign variables with this syntax:
+  [variable] = [value];
+
+So to set a variable named x to 5, do:
+  x = 5;
 
 Every C program needs a main() function, which
 is where the program starts when it is run.
 We're starting you off with one.
-Fill it in with code and press F5.
+
+Press F5 to run your code.
+
 */
+extern int color;
 
 int main ()
 {
   /* Insert your code here */
-  
+  @
   return 0;
 }
