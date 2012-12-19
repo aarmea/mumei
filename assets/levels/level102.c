2c8dfe20a97026e16307d35e7d16bbb63b20421a
Albert Armea <alvin.armea@gmail.com>
2012-12-18 19:01:32 -0500

    Fixed level 102 hints, code, and help text

--- assets/levels/level102.c
+++ assets/levels/level102.c
@@ -1,12 +1,12 @@
-extern int x;
+/*
+Level 1-2: even more variable assignment
+*/
+extern int dir;
 extern int color;
 
 int main ()
 {
-  x = 0;
-  color = 0;
-  /* Your code here */
-
-
+  /* Insert your code here */
+  @
   return 0;
 }
