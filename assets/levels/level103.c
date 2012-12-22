cc5bcc55ce5e2db58e7e3640ab078e442ce7c19c
Albert Armea <alvin.armea@gmail.com>
2012-12-22 01:33:26 -0500

    Made level 103 playable
    Introduced concept of system calls a little earlier to allow for
    slightly hacky direction changing.

--- assets/levels/level103.c
+++ assets/levels/level103.c
@@ -1,13 +1,26 @@
-extern int x;
-extern int y;
-extern int disty;
+/*
+Level 1-3: going up and system calls
 
-int main ()
+The door is not on the ground anymore.
+The robot will need to climb the ladder.
+
+When the robot is at a ladder, setting
+the dir variable to 3 or 4 will move it
+up or down.
+
+The wait(n) function makes the robot wait
+for about n seconds.
+
+So to make the robot wait 5 seconds, do:
+  wait(5);
+
+*/
+int wait(int n);
+extern int dir;
+
+int main()
 {
-  x = 0;
-  y = 0;
-  /* Your code here */
-
-
+  /* Insert your code here */
+  @
   return 0;
 }
