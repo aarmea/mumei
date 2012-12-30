70fd73375e4bf2bdd414967230f3c88b24e4a820
Albert Armea <alvin.armea@gmail.com>
2012-12-29 21:12:12 -0500

    Made level 104 playable
    Added some walls to account for not having taught moveX() yet

--- assets/levels/level104.c
+++ assets/levels/level104.c
@@ -1,15 +1,20 @@
-extern int x;
-extern int y;
-extern int disty;
+/*
+Level 1-4: review
+
+Now apply the concepts from the previous
+levels.
+
+The robot will stop if it hits the top of a
+ladder.
+
+*/
+int wait(int n);
+extern int dir;
 extern int color;
 
 int main ()
 {
-  x = 0;
-  y = 0;
-  color = 0;
-  /* Your code here */
-
-
+  /* Insert your code here */
+  @
   return 0;
 }
